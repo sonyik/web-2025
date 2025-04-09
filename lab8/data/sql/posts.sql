afc680933e536cac46c001606fe9b38e6a11a746
sonyik <dsergey.2006@yandex.ru>
2025-04-09 09:57:01 +0300

    new file:   lab8/data/sql/posts.sql 	new file:   lab8/data/sql/table.sql

---
+++ lab8/data/sql/posts.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name) VALUES ('Ваня Денисов'), ('Лиза Дёмина');
+
+INSERT INTO posts (user_id, photo, text, timestamp, likes) VALUES
+(1, 'lab7/images/post.png', 'Так красиво сегодня на улице! Настоящая зима))', 1712600000, 203),
+(2, 'lab7/images/post.png', 'Сходили с друзьями на выставку, было супер!', 1712603600, 124);
---
+++ lab8/data/sql/table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    photo VARCHAR(255),
+    text TEXT,
+    timestamp INT,
+    likes INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
