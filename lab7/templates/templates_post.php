b54315cfed3db0c552d86fe4cef4c775307aa8af
sonyik <dsergey.2006@yandex.ru>
2025-04-15 22:49:41 +0300

    modified:   lab7/templates/templates_post.php

--- lab7/templates/templates_post.php
+++ lab7/templates/templates_post.php
@@ -1,7 +1,7 @@
 <div class="post">
             <div class="profile">
                 <img class="profile__avatar" src="<?= htmlspecialchars($users[$post['user_id'] - 1]['avatar']) ?>" alt="Аватар">
-                <span class="profile__name"><?= htmlspecialchars($users[$post['user_id'] - 1]['name']) ?></span>
+                <a href="profile.php?id=<?= $post['user_id'] ?>"><?= htmlspecialchars($post['author_name']) ?></a>
                 <button class="profile__edit-btn"><img src="images/Edit.svg" alt="Редактировать"></button>
             </div>
             <img class="post__image" src="<?= htmlspecialchars($post['image']) ?>" alt="Пост">
